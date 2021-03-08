dada6ce842aa4c3793f6dc7d91077dfe14285027
Alka <Alka@LAPTOP-KSUSFIAT>
2021-03-08 23:19:34 +0530

    First commit

---
+++ appdemo/src/main/java/appdemo/Demo.java
@@ -0,0 +1,15 @@
+package appdemo;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     System.out.println("Hello, World!"); 
+	     int a = 10;
+	     System.out.println(++a); 
+	     System.out.println(a++); 
+		 //SalesClass class1=new SalesClass(); 
+		 //class1.display();​​​
+
+	}
+}
